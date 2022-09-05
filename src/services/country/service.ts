46d1079a9bb4d80b6ffa7adf0345de4f695c9136
Mallone Nogueira <mallonenogueira@gmail.com>
2022-09-05 15:30:04 -0300

    feat: change url

--- src/services/country/service.ts
+++ src/services/country/service.ts
@@ -8,7 +8,7 @@ interface Options {
 class CountryService {
   async findAll(options?: Options) {
     const { data } = await api.get<Country[]>(
-      "https://restcountries.eu/rest/v2/all",
+      "https://restcountries.com/v2/all",
       options
     );
     return data;
@@ -16,7 +16,7 @@ class CountryService {
 
   async findByName(name: string, options?: Options) {
     const { data } = await api.get<Country[]>(
-      `https://restcountries.eu/rest/v2/name/${name}`,
+      `https://restcountries.com/v2/name/${name}`,
       options
     );
     return data;
